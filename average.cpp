d9ec6d9cf9887e122c0eaaa11eaebc136f8f69b7
Your Name <you@example.com>
2023-03-03 15:25:29 +0700

    finished

--- average.cpp
+++ average.cpp
@@ -1,1 +1,21 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
 
+int main(int argc,char *argv[]){
+    double sum=0,average=0;
+    int N=argc;
+    if(N==1){
+        cout<<"Please input numbers to find average.\n";
+    }else{
+        for(int i=0;i<N;i++){
+        sum=atoi(argv[i])+sum;
+    }
+        
+        average=sum/(N-1);
+        cout<<"---------------------------------\n";
+        cout<<"Average of "<< N-1<<" numbers = "<<average<<endl;
+        cout<<"---------------------------------\n";
+    }
+    return 0;
+}
